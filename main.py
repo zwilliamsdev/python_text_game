a86307e3d0787373076cb76759cefae5b4a324c9
Zachary Williams <zwilliams.dev@gmail.com>
2023-02-10 14:44:36 -0500

    Refactor debug statements

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     debug("SIMULATE MENU CREATION")
     createMenu("You awake in a cold damp room with only a dim amount of light coming through a small crack in the wall. You notice a chest on the far side of the room.",
                "Look through the crack", "Try to smash the wall", "Open the chest")
+    debug("CHECK EMPTY INVENTORY")
+    getInventoryItems()
     debug("SIMULATE LOOTING CHESTS")
     debug("CHEST COMMON")
     lootChest("common")
--- player.py
+++ player.py
@@ -5,7 +5,7 @@ INVENTORY = []
 
 def addInventoryItem(item):
     INVENTORY.append(item)
-    debug('adding ' + item["name"] + " " + item["description"])
+    debug('adding ' + item["name"])
 
 
 def getInventoryItems():
@@ -13,7 +13,7 @@ def getInventoryItems():
     if itemCount > 0:
         i = 0
         while i < itemCount:
-            print(INVENTORY[i]["name"], INVENTORY[i]["description"])
+            print(INVENTORY[i]["description"])
             i += 1
     else:
         print('Your pockets are empty.')
