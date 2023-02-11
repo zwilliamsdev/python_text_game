18d50b8d422b1df2431ba2ddd3271b664c898e79
Zachary Williams <zwilliams.dev@gmail.com>
2023-02-10 21:30:00 -0500

    added simulation and debug statements again

--- main.py
+++ main.py
@@ -12,25 +12,27 @@ def main():
 
     # Create player object and get player name
     # ply = Player(getPlayerName())
+    simulate('Creating character: Daixso')
     ply = Player("Daixso")
     print(f"You are the great hero {ply.getPlayerName()}!")
 
     # Create initial menu prompt
     createMenu("You awake in a cold damp room with only a dim amount of light coming through a small crack in the wall. You notice a chest on the far side of the room.",
                "Look through the crack", "Try to smash the wall", "Open the chest")
-    lootChest("common")
     # TESTING CORE FUNCTIONALITY
-    ply.loot("chest", "common")
+    simulate('Looting common, rare, epic chests')
     ply.loot("chest", "common")
     ply.loot("chest", "rare")
     ply.loot("chest", "epic")
+    debug('Dumping inventory')
     ply.getInventoryItems()
+    simulate('Hurting player')
     ply.hurtPlayer(50)
+    simulate('Drinking health potion')
     ply.drinkHealthPotion()
     print(f'You now have {ply.getPlayerHealth()} HP.')
-    # ply.healPlayer(25)
-    # ply.getPlayerHealth()
-    # ply.hurtPlayer(100)
+    simulate('Kill player')
+    ply.hurtPlayer(100)
 
 
 def getPlayerName():
