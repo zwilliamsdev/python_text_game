e47b007e66c55a5858a46f66063748244d6d9400
Zachary Williams <zwilliams.dev@gmail.com>
2023-03-01 14:22:05 -0700

    rework of loot to work with new item system

--- loot.py
+++ loot.py
@@ -1,37 +1,35 @@
 from debug import debug, simulate
-import datetime
+from items import itemList
+import time
 import random
+import logging
 
-random.seed(datetime.datetime.now())
+random.seed(time.time())
 
-CHEST_COMMON = [
-    "hPotion",
-    "gold"
-]
+CHEST_COMMON = []
+CHEST_RARE = []
+CHEST_EPIC = []
 
-CHEST_RARE = [
-    "sword",
-    "helmet"
-]
 
-CHEST_EPIC = [
-    "enchanted_necklace",
-    "mystic_ring"
-]
+def createChests():
+    debug("Spawning chests")
+    for i in itemList.keys():
+        if itemList[i].rarity == "common":
+            CHEST_COMMON.append(i)
+        elif itemList[i].rarity == "rare":
+            CHEST_RARE.append(i)
+        elif itemList[i].rarity == "epic":
+            CHEST_EPIC.append(i)
+        else:
+            logging.error("Item " + i + " does not have a rarity")
 
 
 def lootChest(rarity):
-    match rarity:
-        case "common":
-            return createLootChest(CHEST_COMMON)
-        case "rare":
-            return createLootChest(CHEST_RARE)
-        case "epic":
-            return createLootChest(CHEST_EPIC)
-        case _:
-            return createLootChest(CHEST_COMMON)
-
-
-def createLootChest(chest):
-    items = len(chest)
-    return chest[random.randint(0, items-1)]
+    if rarity == "common":
+        return CHEST_COMMON[len(CHEST_COMMON) - 1]
+    elif rarity == "rare":
+        return CHEST_RARE[len(CHEST_RARE) - 1]
+    elif rarity == "epic":
+        return CHEST_EPIC[len(CHEST_EPIC) - 1]
+    else:
+        logging.error("Rarity '" + rarity + "' does not exist")
