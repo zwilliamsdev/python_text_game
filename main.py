af39a805e9ac113efdeab50109fa4b67eed4dbdd
Zachary Williams <zwilliams.dev@gmail.com>
2023-03-01 14:26:25 -0700

    Refactor for new item system

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from menu import greetingMenu, createMenu
-from loot import lootChest
+# from loot import lootChest
 from debug import debug, simulate
+# from player import Player
+from loot import createChests
+# Logging
+import logging
+
 from player import Player
 
 NAME = "Adventure Testing"
 
 
 def main():
+    # Setup game state
+    setupGame()
+
     # Show initial menu to greet player
     greetingMenu(NAME)
 
@@ -14,25 +22,24 @@ def main():
     # ply = Player(getPlayerName())
     simulate('Creating character: Daixso')
     ply = Player("Daixso")
-    print(f"You are the great hero {ply.getPlayerName()}!")
+    # print(f"You are the great hero {ply.getPlayerName()}!")
 
     # Create initial menu prompt
     createMenu("You awake in a cold damp room with only a dim amount of light coming through a small crack in the wall. You notice a chest on the far side of the room.",
                "Look through the crack", "Try to smash the wall", "Open the chest")
-    # TESTING CORE FUNCTIONALITY
     simulate('Looting common, rare, epic chests')
     ply.loot("chest", "common")
     ply.loot("chest", "rare")
     ply.loot("chest", "epic")
     debug('Dumping inventory')
     ply.getInventoryItems()
-    simulate('Hurting player')
-    ply.hurtPlayer(50)
-    simulate('Drinking health potion')
-    ply.drinkHealthPotion()
-    print(f'You now have {ply.getPlayerHealth()} HP.')
-    simulate('Kill player')
-    ply.hurtPlayer(100)
+
+
+def setupGame():
+    debug("Setting up game")
+    # Generate chest loot tables
+    debug("Creating chest loot tables")
+    createChests()
 
 
 def getPlayerName():
@@ -45,3 +52,18 @@ def getPlayerName():
 
 if __name__ == "__main__":
     main()
+
+# # TESTING CORE FUNCTIONALITY
+#     simulate('Looting common, rare, epic chests')
+#     ply.loot("chest", "common")
+#     ply.loot("chest", "rare")
+#     ply.loot("chest", "epic")
+#     debug('Dumping inventory')
+#     ply.getInventoryItems()
+#     simulate('Hurting player')
+#     ply.hurtPlayer(50)
+#     simulate('Drinking health potion')
+#     ply.drinkHealthPotion()
+#     print(f'You now have {ply.getPlayerHealth()} HP.')
+#     simulate('Kill player')
+#     ply.hurtPlayer(100)
